7325bae82bc40a75340133497b71c353b36a0cb0
Sullyvahnn <wojnar.artur1@gmail.com>
2025-01-06 11:49:54 +0100

    added snakeGameAI

---
+++ game.py
@@ -0,0 +1,137 @@
+import pygame
+import random
+from enum import Enum
+from collections import namedtuple
+
+pygame.init()
+font = pygame.font.Font('arial.ttf', 25)
+
+
+#font = pygame.font.SysFont('arial', 25)
+
+class Direction(Enum):
+    RIGHT = 1
+    LEFT = 2
+    UP = 3
+    DOWN = 4
+
+
+Point = namedtuple('Point', 'x, y')
+
+# rgb colors
+WHITE = (255, 255, 255)
+RED = (200, 0, 0)
+BLUE1 = (0, 0, 255)
+BLUE2 = (0, 100, 255)
+BLACK = (0, 0, 0)
+
+BLOCK_SIZE = 20
+SPEED = 0
+RESET_TIME = 100
+
+
+class SnakeGameAI:
+    def __init__(self, w=640, h=480):
+        self.w = w
+        self.h = h
+        # init display
+        self.display = pygame.display.set_mode((self.w, self.h))
+        pygame.display.set_caption('Snake')
+        self.clock = pygame.time.Clock()
+
+        # init game state
+        self.reset()
+
+    def reset(self):
+        self.direction = Direction.RIGHT
+
+        self.head = Point(self.w / 2, self.h / 2)
+        self.snake = [self.head,
+                      Point(self.head.x - BLOCK_SIZE, self.head.y),
+                      Point(self.head.x - (2 * BLOCK_SIZE), self.head.y)]
+
+        self.score = 0
+        self.food = None
+        self._place_food()
+        self.frame_iteration = 0
+
+    def _place_food(self):
+        x = random.randint(0, (self.w - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE
+        y = random.randint(0, (self.h - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE
+        self.food = Point(x, y)
+        if self.food in self.snake:
+            self._place_food()
+
+    def play_step(self, action):
+        self.frame_iteration += 1
+        # [left, straight, right]
+        clock_vise_directions = [Direction.RIGHT, Direction.DOWN, Direction.LEFT, Direction.UP]
+        index = clock_vise_directions.index(self.direction)
+        if action[0] == 1:
+            self.direction = clock_vise_directions[(index-1)%4]
+        if action[2] == 1:
+            self.direction = clock_vise_directions[(index+1)%4]
+        # 2. move
+        self._move(self.direction)  # update the head
+        self.snake.insert(0, self.head)
+        reward = 0
+        # 3. check if game over
+        game_over = False
+        if self.is_collision() or self.frame_iteration >= RESET_TIME*len(self.snake):
+            game_over = True
+            reward = -10
+            return reward, game_over, self.score
+
+        # 4. place new food or just move
+        if self.head == self.food:
+            reward = 10
+            self.score += 1
+            self._place_food()
+        else:
+            self.snake.pop()
+
+        # 5. update ui and clock
+        self._update_ui()
+        self.clock.tick(SPEED)
+        # 6. return game over and score
+        return reward, game_over, self.score
+
+    def is_collision(self, point = None):
+        if point is None:
+            point = self.head
+        # hits boundary
+        if point.x > self.w - BLOCK_SIZE or point.x < 0 or point.y > self.h - BLOCK_SIZE or point.y < 0:
+            return True
+        # hits itself
+        if point in self.snake[1:]:
+            return True
+
+        return False
+
+    def _update_ui(self):
+        self.display.fill(BLACK)
+
+        for pt in self.snake:
+            pygame.draw.rect(self.display, BLUE1, pygame.Rect(pt.x, pt.y, BLOCK_SIZE, BLOCK_SIZE))
+            pygame.draw.rect(self.display, BLUE2, pygame.Rect(pt.x + 4, pt.y + 4, 12, 12))
+
+        pygame.draw.rect(self.display, RED, pygame.Rect(self.food.x, self.food.y, BLOCK_SIZE, BLOCK_SIZE))
+
+        text = font.render("Score: " + str(self.score), True, WHITE)
+        self.display.blit(text, [0, 0])
+        pygame.display.flip()
+
+    def _move(self, direction):
+        x = self.head.x
+        y = self.head.y
+        if direction == Direction.RIGHT:
+            x += BLOCK_SIZE
+        elif direction == Direction.LEFT:
+            x -= BLOCK_SIZE
+        elif direction == Direction.DOWN:
+            y += BLOCK_SIZE
+        elif direction == Direction.UP:
+            y -= BLOCK_SIZE
+
+        self.head = Point(x, y)
+
